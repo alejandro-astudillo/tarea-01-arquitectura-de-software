06240ac5a3c780e6caf65796e4b07f5531b4b1c7
Alejandro Astudillo <103075452+Lex-Programming@users.noreply.github.com>
2024-08-21 14:48:27 -0400

    primer commit

---
+++ README.txt
@@ -0,0 +1,8 @@
+Nombre:               |  ROL USM:     |
+                      |               |
+Alejandro Astudillo   |  201856539-3  |
+______________________|_______________|
+
+INSTRUCCIONES:
+
+ -  Ejecutar: docker-compose up 
---
+++ service1/app/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+import logging
+import time
+import threading
+
+app = FastAPI()
+
+# Configuración del logger
+logging.basicConfig(filename='/var/log/service1/app.log', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def log_message():
+    while True:
+        logger.info("Este es un mensaje de log desde el servidor 1")
+        time.sleep(5)
+
+
+# Iniciar el hilo de logging en segundo plano
+thread = threading.Thread(target=log_message)
+thread.daemon = True
+thread.start()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Servidor 1 funcionando"}
